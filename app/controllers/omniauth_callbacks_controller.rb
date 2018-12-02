c1cc3cc87b2c72867a6586adb8a5878c31cebbe6
zanzivyr <von.developer@gmail.com>
2018-12-02 14:04:31 -0800

    error with omniauth callback, trying this solution on dev server

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,8 +1,8 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def all
-        User = User.from_omniauth(request.env["omniauth.auth"])
+        @user = User.from_omniauth(request.env["omniauth.auth"])
         if user.persisted?
-            sign_in_and_redirect user, notice: "Signed in!"
+            sign_in_and_redirect @user, notice: "Signed in!"
         else
             redirect_to new_user_registration_url
         end
