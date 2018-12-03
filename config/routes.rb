d1bdccdf00172b9535d39ca22781d988fe03608d
zanzivyr <von.developer@gmail.com>
2018-12-02 18:07:38 -0800

    added rails_admin gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"},
                      controllers: {
                         omniauth_callbacks: "omniauth_callbacks", 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_02_213430) do
+ActiveRecord::Schema.define(version: 2018_12_03_020312) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2018_12_02_213430) do
     t.datetime "remember_created_at"
     t.string "provider"
     t.string "uid"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
