6f7bbc209a38878d8d0dca2386d07bedace77fd6
zanzivyr <von.developer@gmail.com>
2018-12-05 18:10:36 -0800

    adding stripe to app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'store/index'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"},
                      controllers: {
@@ -7,7 +8,9 @@ Rails.application.routes.draw do
                      }
   resources :users
   resources :posts
+  resources :charges
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'posts#index'
+  #root to: 'posts#index'
+  root to: 'store#index'
 end
